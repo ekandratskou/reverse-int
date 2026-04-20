ab951445fe0919fb0457455ee69ccf0a7e557e55
Eduard <edyard2002@mail.ru>
2026-04-20 23:13:31 +0300

    init: start task resverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const reversedString = Math.abs(n).toString().split('').reverse().join('');
+  const result = parseInt(reversedString, 10);
+  return result;
 };
